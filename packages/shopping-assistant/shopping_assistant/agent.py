89f1fcacc79e16f57e30a9121cdf8cb30ab2e032
Issa Barake <issabarake@Issas-MacBook-Pro.local>
2024-06-25 20:46:39 -0500

    refactoring data

--- packages/shopping-assistant/shopping_assistant/agent.py
+++ packages/shopping-assistant/shopping_assistant/agent.py
@@ -1,19 +1,13 @@
-from typing import List, Tuple
-
-from ionic_langchain.tool import IonicTool
-from langchain.agents import AgentExecutor, create_openai_tools_agent
 from langchain_core.messages import AIMessage, SystemMessage
 from langchain_core.prompts import (
     ChatPromptTemplate,
     HumanMessagePromptTemplate,
     MessagesPlaceholder,
 )
-from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
 
-tools = [IonicTool().tool()]
-
-llm = ChatOpenAI(temperature=0.5, model_name="gpt-3.5-turbo-1106", streaming=True)
+llm = ChatOpenAI(temperature=0.5, model_name="gpt-4o")
 
 # You can modify these!
 AI_CONTENT = """
@@ -28,20 +22,9 @@ messages = [
     SystemMessage(content=SYSTEM_CONTENT),
     HumanMessagePromptTemplate.from_template("{input}"),
     AIMessage(content=AI_CONTENT),
-    MessagesPlaceholder(variable_name="agent_scratchpad"),
 ]
 
 prompt = ChatPromptTemplate.from_messages(messages)
-agent = create_openai_tools_agent(llm, tools, prompt)
+agent = prompt | llm | StrOutputParser()
 
-
-class AgentInput(BaseModel):
-    input: str
-    chat_history: List[Tuple[str, str]] = Field(
-        ..., extra={"widget": {"type": "chat", "input": "input", "output": "output"}}
-    )
-
-
-agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True).with_types(
-    input_type=AgentInput
-)
+agent_executor = agent
